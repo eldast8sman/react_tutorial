7b44ebbcd838a45e334150c232784e962aad2121
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-01-28 22:39:35 +0100

    Empty Working Environment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { useState } from 'react';
-import './App.css';
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      <button>-</button>
-      <h1>{counter}</h1>
-      <button>+</button>
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
