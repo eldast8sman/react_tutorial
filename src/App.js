05aa0981a623369504d9438c013255f1187b0174
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-01-30 06:13:45 +0100

    Search Input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import { useEffect } from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=6cf30ec1';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response =  await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Lord of the Rings');
+    }, []);
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                />
+            </div>
+        </div>
     );
 }
 
